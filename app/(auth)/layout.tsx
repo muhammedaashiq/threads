c80a0f7bd782bc099eb2d28a7af98e84225066cb
Muhammed Aashiq <109937889+muhammedaashiq@users.noreply.github.com>
2023-08-28 16:55:55 +0530

    Second Commit

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ import '../globals.css'
 
 export const metadata = {
     title: 'Threads',
-    description: 'A Next.js 13 Meta Threads Application'
+    description: 'A Next.js 13 Meta Threads Application' 
 }
 
 const inter = Inter({ subsets: ['latin'] })
